84a431f96e685b131471087bdbb5a748cfc2ba7f
VAIBHAVSINGHRAJPUT <88886371+VAIBHAVSINGHRAJPUT@users.noreply.github.com>
2023-08-10 15:57:27 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+const options = {
+
+  params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+  headers: {
+    'X-RapidAPI-Key': 'process.env.REACT_APP_RAPID_API_KEY',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  
+    return data;
+  };
